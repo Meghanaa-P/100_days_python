35b3b15025c26a40e6242844dccc77e1eedb78c8
Meghanaa-P <meghanapshetty03@gmail.com>
2024-07-11 00:32:33 +0530

    Create day1_TOH.py

---
+++ day1_TOH.py
@@ -0,0 +1,11 @@
+def TowerOfHanoi(n , source, destination, auxiliary):
+    if n==1:
+        print (f"Move disk 1 from source {source} to destination {destination}")
+        return
+    TowerOfHanoi(n-1, source, auxiliary, destination)
+    print (f"Move disk {n} from source {source} to destination {destination}")
+    TowerOfHanoi(n-1, auxiliary, destination, source)
+         
+
+n = 4
+TowerOfHanoi(n,'A','B','C')
