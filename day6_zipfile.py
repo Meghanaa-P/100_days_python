afaae558fb07c87f4c1ce31fd1a2a25ddd0783bc
Meghanaa-P <meghanapshetty03@gmail.com>
2024-07-15 21:08:24 +0530

    Update day6_zipfile.py

--- day6_zipfile.py
+++ day6_zipfile.py
@@ -1,12 +1,58 @@
-import zipfile
-import os
+from zipfile import ZipFile 
+import os 
 
-def zip_file(file_path, zip_path):
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        zipf.write(file_path, os.path.basename(file_path))
-    print(f"File {file_path} zipped successfully to {zip_path}")
+def get_all_file_paths(directory): 
+ 
+	file_paths = [] 
 
-# Example usage
-file_to_zip = 'example.txt'  # Replace with your file path
-zip_destination = 'example.zip'  # Replace with your desired zip file path
-zip_file(file_to_zip, zip_destination)
+	for root, directories, files in os.walk(directory): 
+		for filename in files:  
+			filepath = os.path.join(root, filename) 
+			file_paths.append(filepath) 
+
+	# returning all file paths 
+	return file_paths		 
+
+def zip():  
+	directory = input("enter directory name to zip: ")
+
+	file_paths = get_all_file_paths(directory) 
+
+	print('Following files will be zipped:') 
+	for file_name in file_paths: 
+		print(file_name) 
+
+	# writing files to a zipfile 
+	with ZipFile('my_python_files.zip','w') as zip: 
+		# writing each file one by one 
+		for file in file_paths: 
+			zip.write(file) 
+
+	print('All files zipped successfully!')		 
+
+def main():
+	a=input("Do you want to zip a file y/n: ")
+	if a=='y':
+		zip()
+		
+	b=input("Do you want to unzip a file y/n: ")	
+	if b=='y':
+		unzip()
+	else:
+		return 	
+		
+
+def unzip(): 
+    file_name = input("Enter file name to unzip: ")
+  
+    with ZipFile(file_name, 'r') as zip: 
+        zip.printdir() 
+  
+    # extracting all the files 
+    print('Extracting all the files now...') 
+    zip.extractall() 
+    print('Done!') 
+
+if __name__ == "__main__": 
+    main() 
+
